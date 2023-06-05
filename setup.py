9deacc8882f718d1705aff5fafcd6e29ab506f92
maneroto <maneroto@hotmail.com>
2023-06-05 10:08:54 -0600

    Remove specific exception from authenticate

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from config.settings import *
 def authenticate():
   try:
     kaggle.api.authenticate()
-  except IOError:
+  except:
     dotenv.load_dotenv()
     kaggle.api.authenticate_from_env()
 
